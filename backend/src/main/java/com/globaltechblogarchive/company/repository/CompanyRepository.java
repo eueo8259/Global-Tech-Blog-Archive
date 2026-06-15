276f5274111c2274ea489fe8f7641ab954444ee4
eueo8259 <eueo8259@gmail.com>
2026-06-15 23:42:21 +0900

    feat: 회사 리스트 반환 API 구현

--- backend/src/main/java/com/globaltechblogarchive/company/repository/CompanyRepository.java
+++ backend/src/main/java/com/globaltechblogarchive/company/repository/CompanyRepository.java
@@ -1,6 +1,8 @@
 package com.globaltechblogarchive.company.repository;
 
 import com.globaltechblogarchive.company.domain.Company;
+
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     List<Company> findByCompanyKeyIn(Collection<String> companyKeys);
+
+    List<Company> findAllByOrderByCompanyNameAsc();
 }
