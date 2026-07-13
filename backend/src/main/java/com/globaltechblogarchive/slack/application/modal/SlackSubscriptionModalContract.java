1aba33b73398eec7cd90ce87f70aad6eb65fddc2
eueo8259 <eueo8259@gmail.com>
2026-07-13 15:18:06 +0900

    feat: Slack 구독 모달 생성 기능 추가

---
+++ backend/src/main/java/com/globaltechblogarchive/slack/application/modal/SlackSubscriptionModalContract.java
@@ -0,0 +1,11 @@
+package com.globaltechblogarchive.slack.application.modal;
+
+public final class SlackSubscriptionModalContract {
+
+    public static final String CALLBACK_ID = "subscription_settings";
+    public static final String COMPANY_BLOCK_ID = "company_subscriptions";
+    public static final String COMPANY_ACTION_ID = "selected_companies";
+
+    private SlackSubscriptionModalContract() {
+    }
+}
